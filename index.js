d42dd2be723e25c5ff7bce4b683387be27e0588e
Earts-vinay <vinay.r@entitledarts.com>
2024-07-08 19:17:55 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // scripts.js
+const aboutUsSection = document.querySelector('.About_us');
+const aboutUsHeading = document.querySelector('.about-us-heading');
+const banner = document.querySelector(".banner");
 
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
@@ -9,9 +12,32 @@ window.addEventListener('scroll', function() {
     }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const banner = document.querySelector(".banner");
-    banner.style.animation = "slide-in 1s ease-out forwards";
-});
 
+
+function checkScroll() {
+    if (window.scrollY < 0) {
+        banner.classList.add("slide-in");
+    } else {
+        banner.classList.remove("slide-in");
+    }
+}
+
+window.addEventListener("scroll", checkScroll);
+
+// Trigger the function on page load in case the page is already at the top
+checkScroll();
+
+
+window.addEventListener('scroll', function() {
+    const sectionTop = aboutUsHeading.getBoundingClientRect().top;
+    console.log("lll",sectionTop);
+    const isInViewport = sectionTop < window.innerHeight && sectionTop >= 80;
+
+    if (isInViewport) {
+        aboutUsHeading.classList.add('underline-animate');
+    }else{
+        aboutUsHeading.classList.remove('underline-animate');
+
+    }
+});
 
