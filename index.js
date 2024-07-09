b54610c2f773345ab3bb74b44c726d6a78cd27ef
Earts-vinay <vinay.r@entitledarts.com>
2024-07-09 19:22:51 +0530

    chamges

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // scripts.js
 const aboutUsSection = document.querySelector('.About_us');
 const aboutUsHeading = document.querySelector('.about-us-heading');
+
+const whychooseUsSection = document.querySelector('.why-choose-us');
+const whychooseUsHeading = document.querySelector('.why-choose-us-heading');
+
+const projectSection = document.querySelector('.project');
+const projectheading = document.querySelector('.project-heading');
+
+
 const banner = document.querySelector(".banner");
 
 window.addEventListener('scroll', function() {
@@ -27,7 +35,7 @@ window.addEventListener("scroll", checkScroll);
 // Trigger the function on page load in case the page is already at the top
 checkScroll();
 
-
+//About Us heading
 window.addEventListener('scroll', function() {
     const sectionTop = aboutUsHeading.getBoundingClientRect().top;
     console.log("lll",sectionTop);
@@ -41,3 +49,72 @@ window.addEventListener('scroll', function() {
     }
 });
 
+//why-choose-us heading
+window.addEventListener('scroll', function() {
+    const whychooseUs = whychooseUsHeading.getBoundingClientRect().top;
+
+    console.log("lll",whychooseUs);
+    const whychoose = whychooseUs < window.innerHeight && whychooseUs >= 80;
+
+    if (whychoose) {
+        whychooseUsHeading.classList.add('whychooseus-underline-animate');
+    }else{
+        whychooseUsHeading.classList.remove('whychooseus-underline-animate');
+
+    }
+});
+
+//project heading
+window.addEventListener('scroll', function() {
+    const projects = projectheading.getBoundingClientRect().top;
+
+    
+    const projectk = projects < window.innerHeight && projects >= 80;
+
+    if (projectk) {
+        projectheading.classList.add('projects-underline-animate');
+    }else{
+        projectheading.classList.remove('projects-underline-animate');
+
+    }
+});
+
+
+//lottie icons
+//eco-production
+document.addEventListener('DOMContentLoaded', function() {
+    lottie.loadAnimation({
+        container: document.getElementById('lottie-container'), // the DOM element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'images/icons/ecoProduction.json'
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    lottie.loadAnimation({
+        container: document.getElementById('smart-devices'), // the DOM element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'images/icons/smartDevices.json'
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    lottie.loadAnimation({
+        container: document.getElementById('urban'), // the DOM element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'images/icons/urban.json'
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    lottie.loadAnimation({
+        container: document.getElementById('green'), // the DOM element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'images/icons/green.json'
+    });
+});
